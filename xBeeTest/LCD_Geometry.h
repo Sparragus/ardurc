b3a3bb5ee0b887510148d87a51fb7a0656bde2b4
Richard B. Kaufman-Lopez <richardbkaufman@gmail.com>
2010-12-08 14:37:59 -0400

    Added fixed xBeeTest directory and files

---
+++ xBeeTest/LCD_Geometry.h
@@ -0,0 +1,10 @@
+
+//********************************************************************
+//
+//				General Function Definitions
+//
+//********************************************************************
+
+void LCDSetLine(int x0, int y0, int x1, int y1, int color);
+void LCDSetRect(int x0, int y0, int x1, int y1, unsigned char fill, int color);
+void LCDSetCircle(int x0, int y0, int radius, int color);
